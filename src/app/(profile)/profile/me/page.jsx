63ff48d1f3fb28c3be93cb05c47c96c49855e390
fatiseddigh <seddighfati@gmail.com>
2023-07-18 16:03:09 +0330

    feat: create update user profile

--- src/app/(profile)/profile/me/page.jsx
+++ src/app/(profile)/profile/me/page.jsx
@@ -3,24 +3,40 @@
 import Loading from "@/common/Loading";
 import TextField from "@/common/TextField";
 import { useGetUser } from "@/hooks/useAuth";
+import { updateProfile } from "@/services/authServices";
 import { includeObj } from "@/utils/objectUtils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const Profile = () => {
   const [formData, setFormData] = useState({});
   const { data, isLoading } = useGetUser();
   const { user } = data || {};
-
+  const queryClient = useQueryClient();
+  const { isLoading: isUpdating, mutateAsync } = useMutation({
+    mutationFn: updateProfile,
+  });
   const includesKey = ["name", "email", "phoneNumber", "biography"];
   useEffect(() => {
     if (user) setFormData(includeObj(user, includesKey));
   }, [user]);
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { message } = await mutateAsync(formData);
+      queryClient.invalidateQueries({ queryKey: ["get-user"] });
+      toast.success(message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
   if (isLoading) return <Loading />;
   return (
     <div className="max-w-sm">
-      <h1>User Info</h1>
-      <form action="">
+      <h1 className="text-xl font-bold mb-4">User Info</h1>
+      <form onSubmit={submitHandler} className="space-y-5">
         {Object.keys(includeObj(user, includesKey)).map((key) => {
           return (
             <TextField
@@ -34,6 +50,13 @@ const Profile = () => {
             />
           );
         })}
+        <button
+          disabled={isUpdating}
+          type="submit"
+          className="btn btn--primary w-full"
+        >
+          {isUpdating ? "LOADING ..." : "send"}
+        </button>
       </form>
     </div>
   );
