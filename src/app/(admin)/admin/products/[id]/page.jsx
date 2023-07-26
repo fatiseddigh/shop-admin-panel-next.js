53d8ded65cbba345212d5667ae260778c6d439c9
fatiseddigh <seddighfati@gmail.com>
2023-07-26 11:53:18 +0330

    feat: fetch product id

--- src/app/(admin)/admin/products/[id]/page.jsx
+++ src/app/(admin)/admin/products/[id]/page.jsx
@@ -1,10 +1,19 @@
 "use client";
 
+import Loading from "@/common/Loading";
+import { useGetProductById } from "@/hooks/useProducts";
 import { useParams } from "next/navigation";
 
 function Page() {
   const { id } = useParams();
-  return <div>page {id}</div>;
+  const { data, isLoading } = useGetProductById(id);
+  const { product } = data || {};
+  if (isLoading) return <Loading />;
+  return (
+    <div>
+      <h1 className="mb-4 font-bold text-xl">{product.title}</h1>
+    </div>
+  );
 }
 
 export default Page;
